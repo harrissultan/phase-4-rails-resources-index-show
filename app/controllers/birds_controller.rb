2bfcc24a5d1e3c88b398b722d6a6127b77113767
Harris Sultan <harris_sultan@outlook.com>
2022-03-15 14:42:08 -0400

    commit tree 1ea8bbf4c1fee9a572e8b352c8ed372a2cc4a5b7

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,14 @@
+class BirdsController < ApplicationController
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "Birds not found"}
+        end
+    end
+end
